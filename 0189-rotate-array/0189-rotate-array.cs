a2d5569138bb3f17d1049d9f32c6afe1e537fd58
Pranav Bajpai <142435414+PranavBajpai07@users.noreply.github.com>
2025-11-25 19:32:58 +0530

    Time: 0 ms (100%), Space: 66 MB (58.13%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cs
+++ 0189-rotate-array/0189-rotate-array.cs
@@ -1,13 +1,7 @@
-class Solution {
+public class Solution {
     public void Rotate(int[] nums, int k) {
-        int n = nums.Length;
-        k = k % n;
-        int[] res = new int[n];
-        for (int i = 0; i < n; i++) {
-            res[(i + k) % n] = nums[i];
-        }
-        for (int i = 0; i < n; i++) {
-            nums[i] = res[i];
-        }
+        Array.Reverse(nums);
+        Array.Reverse(nums,0,(k%nums.Length));
+        Array.Reverse(nums,(k%nums.Length),nums.Length-(k%nums.Length));
     }
 }
