4fd9bfd842b3504bba6985d427428cdacd0ed4d9
Pranav Bajpai <142435414+PranavBajpai07@users.noreply.github.com>
2025-11-25 20:22:34 +0530

    Time: 4 ms (45.09%), Space: 41.6 MB (93.18%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cs
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cs
@@ -1,10 +1,51 @@
 public class Solution {
     public string ReverseWords(string s) {
-        s = s.Trim();
+        if( string.IsNullOrWhiteSpace(s)) {
+            return string.Empty; // Retrun empty string if input is empty or only spaces
+        }
 
-        string[] words = s.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+        char[] chars = s.ToCharArray();
 
-        Array.Reverse(words);
-        return string.Join(" ", words);
+        Reverse(chars, 0, chars.Length - 1);
+
+        ReverseEachWord(chars);
+
+        return CleanSpaces(chars);
+
+    }
+
+    private void Reverse(char[] chars, int left, int right) {
+        while(left < right) {
+            char temp = chars[left];
+            chars[left++] = chars[right];
+            chars[right--] = temp;
+        }
+    }
+
+    private void ReverseEachWord(char[] chars) {
+        int n = chars.Length;
+        int start = 0, end = 0;
+
+        while (start < n) {
+            while (start < n && chars[start] == ' ') start++;
+            end = start;
+            while (end < n && chars[end] != ' ') end++;
+            Reverse(chars, start, end - 1);
+            start = end;
+        }
+    }
+
+    private string CleanSpaces(char[] chars) {
+        int n = chars.Length;
+        int i = 0, j = 0;
+
+        while (j < n){
+            while (j < n && chars[j] == ' ') j++;
+            while (j < n && chars[j] != ' ') chars[i++] = chars[j++];
+            while (j < n && chars[j] == ' ') j++;
+            if (j < n) chars[i++] = ' ';
+        }
+
+        return new string(chars, 0, i);
     }
 }
