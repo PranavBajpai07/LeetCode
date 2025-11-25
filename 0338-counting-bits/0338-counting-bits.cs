603511c5b854cc17d372caf841d88b1ac0eece43
Pranav Bajpai <142435414+PranavBajpai07@users.noreply.github.com>
2025-11-25 22:21:21 +0530

    Time: 1 ms (48.36%), Space: 43.7 MB (39.05%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cs
+++ 0338-counting-bits/0338-counting-bits.cs
@@ -1,11 +1,25 @@
 public class Solution {
-    public int[] CountBits(int n) {
-        int[] ans = new int[n + 1];
+    public int[] CountBits(int n)
+{
+    int[] ans = new int[n + 1];
 
-        for (int i = 1; i <= n; i++) {
-            ans[i] = ans[i >> 1] + (i & 1);
-        }
-        
-        return ans;
+    for (int i = 0; i <= n; i++)
+    {
+        ans[i] = CountOnes(i);
     }
+
+    return ans;
+}
+
+private int CountOnes(int x)
+{
+    int count = 0;
+    while (x > 0)
+    {
+        x &= (x - 1);
+        count++;
+    }
+    return count;
+}
+
 }
