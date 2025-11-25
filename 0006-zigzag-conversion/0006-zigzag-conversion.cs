b609151647857111d1056ca3dc97c4f01294d1a9
Pranav Bajpai <142435414+PranavBajpai07@users.noreply.github.com>
2025-11-25 19:17:23 +0530

    Time: 10 ms (55.62%), Space: 53.5 MB (41.45%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cs
@@ -0,0 +1,30 @@
+public class Solution {
+    public string Convert(string s, int numRows) {
+         if(numRows == 1 || s.Length <= numRows) return s;
+
+        List<StringBuilder> rows = new List<StringBuilder>();
+        for(int i = 0; i < Math.Min(s.Length, numRows); i++){
+            rows.Add(new StringBuilder());
+        }
+
+        int currentRow = 0;
+        bool goingDown = false;
+
+        foreach(char c in s){
+            rows[currentRow].Append(c);
+
+            if (currentRow == 0 || currentRow == numRows - 1){
+                goingDown = !goingDown;
+            }
+
+            currentRow += goingDown ? 1 : -1;    
+        }
+
+        StringBuilder result = new StringBuilder();
+        foreach(var row in rows){
+            result.Append(row);
+        }
+
+        return result.ToString();
+    }
+}
